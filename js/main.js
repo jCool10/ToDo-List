14ad8403ed3a80383ce61a42958bb8c644d5552d
Hoàng Ngọc Lộc <84013644+jCool10@users.noreply.github.com>
2022-03-24 09:44:25 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,43 +1,42 @@
-//Import lớp đối tượng
+//Import class
 import { ToDo } from "./todo.js";
 import { ToDoList } from "./todoList.js";
 
 let todoList = new ToDoList();
 let completeList = new ToDoList();
-//Hàm rút gọn cú pháp getElementById
+
 const getELE = id => {
   return document.getElementById(id);
 }
 
-//Hàm thêm todo
+//Function add todo
 const addToDo = () => {
   let txtToDo = getELE("newTask").value;
   let ulToDo = getELE("todo");
-
+  
   if (txtToDo != "") {
     let td = new ToDo(txtToDo, "todo");
     todoList.addToDo(td);
   }
-  //gọi hàm
+  
   showToDoList(ulToDo);
-
+  saveToDoList();
   getELE("newTask").value = "";
 }
-
+// click button add todo
 getELE("addItem").addEventListener("click", () => {
   addToDo();
+  saveToDoList();
 });
 
-//Hàm hiển thị todo
-//Khai báo hàm
 const showToDoList = (ulToDo) => {
   ulToDo.innerHTML = todoList.renderToDo();
 }
-
 const showCompleteList = (ulCompleted) => {
   ulCompleted.innerHTML = completeList.renderToDo();
 }
-//Hàm delete todo
+
+//function delete todo
 const deleteToDo = (e) => {
   let tdIndex = e.currentTarget.getAttribute("data-index");
   let status = e.currentTarget.getAttribute("data-status");
@@ -46,14 +45,14 @@ const deleteToDo = (e) => {
   if (status == "todo") {
     todoList.removeToDo(tdIndex);
     showToDoList(ulToDo);
+    saveToDoList();
   } else if (status == "completed") {
     completeList.removeToDo(tdIndex);
     showCompleteList(ulCompleted);
+    saveToDoList();
   } else {
     alert("Cannot delete todo!");
   }
-
-
 }
 
 window.deleteToDo = deleteToDo;
@@ -71,12 +70,14 @@ const completeToDo = (e) => {
     moveToDo(todoList, completeList, objToDo, tdIndex);
     showToDoList(ulToDo);
     showCompleteList(ulCompleted);
+    saveToDoList();
   } else if (status == "completed") {
     let undoItem = completeList.tdList.slice(tdIndex, tdIndex + 1);
     let objToDo = new ToDo(undoItem[0].textTodo, "todo");
     moveToDo(completeList, todoList, objToDo, tdIndex);
     showToDoList(ulToDo);
     showCompleteList(ulCompleted);
+    saveToDoList();
   } else {
     alert("Cannot move todo !");
   }
@@ -90,7 +91,6 @@ const moveToDo = (depart, arrival, obj, tdIndex) => {
 
   //Add todo to arrival
   arrival.addToDo(obj);
-
 }
 
 const sortASC = () => {
@@ -109,15 +109,46 @@ const sortDES = () => {
 
 window.sortDES = sortDES;
 
-
 function date(){
   var today = new Date();
   var years = today.getFullYear();
   var month = today.getMonth() + 1;
   var day = today.getDate();
-
   document.getElementById("years").innerHTML = years;
   document.getElementById("month").innerHTML = month;
   document.getElementById("day").innerHTML = day;
 }
 setInterval(date, 1000);
+
+function saveToDoList() {
+  let todolist = document.querySelectorAll('li');
+  let todoStorage = [];
+  todolist.forEach((item) => {
+    let value = item.querySelector('span').innerText;
+    let status = item.querySelector('button').getAttribute('data-status');
+    let obj = {
+      value: value,
+      status: status
+    }
+    todoStorage.push(obj); 
+  });
+  localStorage.setItem('todolist', JSON.stringify(todoStorage));
+}
+// render todo list from local storage when page load 
+window.addEventListener('load', () => {
+  let todolist = JSON.parse(localStorage.getItem('todolist'));
+  if (todolist) {
+    todolist.forEach((item) => {
+      let obj = new ToDo(item.value, item.status);
+      if (item.status == "todo") {
+        todoList.addToDo(obj);
+      } else if (item.status == "completed") {
+        completeList.addToDo(obj);
+      }
+    });
+  }
+  let ulToDo = getELE("todo");
+  let ulCompleted = getELE("completed");
+  showToDoList(ulToDo);
+  showCompleteList(ulCompleted);
+});
